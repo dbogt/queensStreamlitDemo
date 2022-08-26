97a1cacc0ab50fc1689c1d60b40ad469cbd3c27c
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-08-26 15:52:22 -0400

    added ipos table

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ import pandas as pd
 st.title("Demo Dashbord")
 st.write("Example dashboard in Marquee class for Queen's University")
 
+#st.write is similar to print() in python
+
 categories = ['A', 'B', 'C']
 st.multiselect('pick an option', categories)
 
 st.sidebar.button("Click me!")
+
+url = "https://www.iposcoop.com/last-100-ipos/"
+df = pd.read_html(url)[0]
+
+st.write(df)
+
+st.dataframe(df)

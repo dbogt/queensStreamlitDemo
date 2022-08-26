195acf46763fed2f8553263be6840ac54985003a
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-08-26 15:47:28 -0400

    added new components

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import pandas as pd
 
 st.title("Demo Dashbord")
 st.write("Example dashboard in Marquee class for Queen's University")
+
+categories = ['A', 'B', 'C']
+st.multiselect('pick an option', categories)
+
+st.sidebar.button("Click me!")
